4ccd407068d59f9e3d2af4df68a5171806ee1161
Martin Bro Mikkelsen <martinbromikkelsen@gmail.com>
2018-07-05 20:34:51 +0200

    oprettet en docs mappe

--- src/components/Bezier.vue
+++ src/components/Bezier.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="bezier">
       <p>Bezier.vue</p>
-      <svg viewBox="0 -100 100 100">
+      <svg  viewBox="0 -100 200 100" preserveAspectRatio="none">
             
-            <circle v-for="(q,index) in Q" v-bind:cx="Q[index][0]" v-bind:cy="-q[1]" r="0.5"></circle>
+            <circle v-for="(q,index) in Q" v-bind:cx="Q[index][0]" v-bind:cy="-q[1]" r="0.5" stroke="orange" stroke-width="0.1" fill="none"></circle>
             
-        <path  :d = "cubicBezier(Q)" fill="none" stroke="red" stroke-width="0.2" />
-        <path  :d = "cubicBezierLiniesegment(Qlin)" fill="none" stroke="black" stroke-width="0.2" />
+        <path  :d = "cubicBezier(Q)" fill="none" stroke="red" stroke-width="0.1" />
+        <path  :d = "cubicBezierLiniesegment(Qlin)" fill="none" stroke="black" stroke-width="0.1" />
         <path  :d = "quadraticBezier(Q1)" fill="none" stroke="blue" stroke-width="0.1" />
       </svg>
     <p>{{cubicBezierLiniesegment(Qlin)}}</p>
@@ -22,18 +22,18 @@ export default {
   },
    data: function() {
       return {
-        Q: [[0,70],
-            [20,67.6],
-            [40,50.8],
-            [60,5.2]
+        Q: [[0,0],
+            [5,39.37250],
+            [20,39.74],
+            [30,79.11]
         ],
-        Q1: [[0,70],
-            [40,50.8],
-            [60,5.2]
+        Q1: [[0,0],
+            [10,49.97],
+            [30,79.11]
         ],
         Qlin: [
-          [0,70,0], //x0, f(x0), f´(x0)
-          [60,5.2,-3.24] ////x0, f(x0), f´(x0)
+          [5,39.37250,4.5395], //x0, f(x0), f´(x0)
+          [30,79.11, 10.077] ////x0, f(x0), f´(x0)
         ],
       
         // punkter: [
@@ -62,14 +62,35 @@ export default {
 
       },
     cubicBezier: function(Q) {
-      
-      
-      const A = [ 
-        [ 1, 0, 0, 0],
-          [ -5/6, 3, -1.5, 1/3],
-          [ 1/3, -1.5, 3, -5/6],
-          [ 0, 0, 0, 1 ]
+        const s1 = 0
+        const s2 = (Q[1][0]-Q[0][0])/(Q[3][0]-Q[0][0])
+        const s3 = (Q[2][0]-Q[0][0])/(Q[3][0]-Q[0][0])
+        const s4 = 1
+        
+        const M = [
+              [1, 0, 0, 0],
+              [-3, 3, 0, 0],
+              [3, -6, 3, 0],
+              [-1, 3, -3, 1]
         ]
+
+        const T = [
+            [1, s1, s1*s1, s1*s1*s1],
+            [1, s2, s2*s2, s2*s2*s2],
+            [1, s3, s3*s3, s3*s3*s3],
+            [1, s4, s4*s4, s4*s4*s4],
+        ]
+        
+        const TtT =  math.multiply(math.transpose(T),T)
+        const TtTinvTt = math.multiply(math.inv(TtT),math.transpose(T))
+        const A = math.multiply(math.inv(M),TtTinvTt)
+      
+      // const A = [ 
+      //   [ 1, 0, 0, 0],
+      //     [ -5/6, 3, -1.5, 1/3],
+      //     [ 1/3, -1.5, 3, -5/6],
+      //     [ 0, 0, 0, 1 ]
+      //   ]
         const p =  math.multiply(A, Q)
         
         const kurve = `M ${p[0][0]},${-1*p[0][1]} C${p[1][0]},${-1*p[1][1]} ${p[2][0]},${-1*p[2][1]} ${p[3][0]},${-1*p[3][1]} `
